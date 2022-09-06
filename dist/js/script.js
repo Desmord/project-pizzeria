3b3c741b3e1c6511e3cb6faf449259a32f384450
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-09-06 13:42:36 +0200

    Add fetch data from server

--- dist/js/script.js
+++ dist/js/script.js
@@ -76,6 +76,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
     // CODE ADDED END
   };
 
@@ -410,7 +415,7 @@
       }
     }
 
-    remove(detail,eventTarget) {
+    remove(detail, eventTarget) {
       const thisCart = this;
       const filtredProducts = thisCart.products.filter(product => product.id != detail.id ? true : false);
 
@@ -508,13 +513,28 @@
       const thisApp = this;
 
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function () {
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+
+      fetch(url)
+        .then(function (rawResponse) {
+          return rawResponse.json();
+        })
+        .then(function (parsedResponse) {
+          console.log(`parsedResponse`, parsedResponse)
+
+          thisApp.data.products = parsedResponse;
+          thisApp.initMenu();
+        });
+
+      console.log(`thisApp.data`, JSON.stringify(thisApp.data));
+
     },
 
     initCart: function () {
@@ -526,14 +546,8 @@
 
     init: function () {
       const thisApp = this;
-      // console.log('*** App starting ***');
-      // console.log('thisApp:', thisApp);
-      // console.log('classNames:', classNames);
-      // console.log('settings:', settings);
-      // console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       this.initCart();
     },
   };
